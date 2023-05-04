8f9bc4425fdafe2452d12689d5d1737738075720
matval <matteo.vallar@epfl.ch>
2023-05-04 16:18:45 +0200

    Tool to count the rows in all the tables in your sql database

---
+++ count_rows_tables.sql
@@ -0,0 +1,9 @@
+-- This query allows to store in a database the name of all the tables and their rows
+DROP TABLE tables_rows;
+CREATE TABLE tables_rows(table_name VARCHAR(20), n_rows INT);
+INSERT INTO tables_rows
+SELECT table_name, table_rows
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'test_db';
+DELETE FROM tables_rows WHERE table_name='tables_rows';
+SELECT * FROM tables_rows;
